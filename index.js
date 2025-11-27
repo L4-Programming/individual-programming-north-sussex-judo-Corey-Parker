deed68c950463269681721bb309dd604df2c732b
Corey Parker <238495322+Corey-Parker@users.noreply.github.com>
2025-11-27 16:24:06 +0000

    Started to add dynamic filling of select

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 import { validateInput } from "./validateInput.js";
 
 let form = document.querySelector("form");
+
+
+categoryObject = {
+  Heavy : 750,
+	LHeavy : 100,
+	Middle : 90,
+	LMiddle : 81,
+	Light : 73,
+	LLight : 66
+}
+
+Object.keys(categoryObject).forEach(key => {
+  
+ categorySelect = document.querySelector("#categoryInput");
+ categorySelect.options[categorySelect.options.length] = new Option(key +""+myObj[key], key);
+
+})
+
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   let inputsObject = {
