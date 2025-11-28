1720e1b2739554420ca2e4e83029b4c420dd8168
Corey-Parker <238495322+Corey-Parker@users.noreply.github.com>
2025-11-28 13:13:41 +0000

    Added cost display, cost object values are now always 2dp

--- displayResults.js
+++ displayResults.js
@@ -16,11 +16,11 @@ clearResults()
 let coachingcost = 10; let competitioncost = 22;
 
 let costObject = {
-  session : plansObj[inObj.plan][1] * plansObj[inObj.plan][0] * 4,
-  coaching : coachingcost * inObj.coaching,
-  competition : competitioncost * inObj.competition,
+  session : parseFloat(plansObj[inObj.plan][1] * plansObj[inObj.plan][0] * 4).toFixed(2),
+  coaching : parseFloat(coachingcost * inObj.coaching).toFixed(2),
+  competition : parseFloat(competitioncost * inObj.competition).toFixed(2)
 }
-costObject.sum = costObject.session + costObject.coaching + costObject.competition
+costObject.sum = parseFloat(costObject.session + costObject.coaching + costObject.competition).toFixed(2)
 
 console.log(costObject)
 
@@ -31,32 +31,71 @@ let htmlAdd =
         <h2>Athlete Information</h2>
       </div>
       
-  <dl>
-    <dd >${inObj.name}</dd>
 
-    <dd >${inObj.plan}</dd>
+    <div class = "container">
+    <div>Name:</div>
+    <div>${inObj.name}</div>
+    </div>
 
-    <dd> Category: ${inObj.cat} (<${catObj[inObj.cat]}) </dd>
+    <div class = "container">
+    <div>Plan:</div>
+    <div>${inObj.plan}</div>
+    </div>
 
-    <dd >Current Weight: ${inObj.weight}</dd>
+    <div class = "container">
+    <div>Category:</div>
+    <div> ${inObj.cat} (<${catObj[inObj.cat]}) </div>
+    </div>
+
+    <div class = "container">
+    <div>Current Weight:</div>
+    <div>${inObj.weight}</div>
+    </div>
 `
-if(inObj.competition != ""){htmlAdd = htmlAdd + `<dd >Competitions: ${inObj.competition}</dd>`}
-if(inObj.coaching != ""){htmlAdd = htmlAdd + `<dd >Coaching: ${inObj.coaching}</dd>`}
-htmlAdd = htmlAdd + `</dl> `
+if(inObj.competition != ""){htmlAdd = htmlAdd + `<p >Competitions: ${inObj.competition}</p>`}
+if(inObj.coaching != ""){htmlAdd = htmlAdd + `<p >Coaching: ${inObj.coaching}</p>`}
+
 document.getElementById("output").innerHTML = htmlAdd
 
 htmlAdd =
  `
   <dl>
-    <dd >Costs</dd>
+      <div class="athlete-info">
+        <h2>Costs</h2>
+      </div>
 
-    <dd>Sessions:  ${costObject.session}</dd>
+    <div class = "container">
+    <div>Sessions: </div>
+    <div>£${costObject.session}</div>
+    </div>
 
-    <dd> Coaching: ${costObject.coaching}  </dd>
-
-    <dd >Competition: ${costObject.competition}</dd>
-    
-    <dd >Total for the month: ${costObject.sum}</dd>
 `
+
+if(inObj.coaching != ""){htmlAdd = htmlAdd +
+`
+    <div class = "container">
+    <div>Coaching:</div>
+    <div>£${costObject.coaching} </div>
+    </div>
+`};
+
+if(inObj.competition != ""){htmlAdd = htmlAdd +
+`
+    <div class = "container">
+    <div>Competition:</div>
+    <div>£${costObject.competition}</div>
+    </div>
+`};
+
+htmlAdd = htmlAdd +
+`
+    <div class = "container">
+    <div>Total for the month:</div>
+    <div>£${costObject.sum}</div>
+    </div>
+
+`;
+
+document.getElementById("costs").innerHTML = htmlAdd
 
 }
