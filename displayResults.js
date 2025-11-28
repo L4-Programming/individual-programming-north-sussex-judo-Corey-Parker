b8dbdb58e76621307b7c16e9e7472c56591358cd
Corey-Parker <238495322+Corey-Parker@users.noreply.github.com>
2025-11-28 10:49:55 +0000

    Added cost calculation and improved display

--- displayResults.js
+++ displayResults.js
@@ -11,31 +11,42 @@ export function displayResults(inObj,catObj, plansObj){
     coaching: document.querySelector("#coachingInput").value,
     competition: document.querySelector("#competitionInput").value
   } */
-    clearResults()
-    console.log("displayResults")
+clearResults()
 
+let coachingcost = 10; let competitioncost = 22;
 
+let costObject = {
+  session : plansObj[inObj.plan][1] * plansObj[inObj.plan][0] * 4,
+  coaching : coachingcost * inObj.coaching,
+  competition : competitioncost * inObj.competition,
+}
+costObject.sum = costObject.session + costObject.coaching + costObject.competition
 
-    document.getElementById("output").innerHTML = 
+console.log(costObject)
 
+document.getElementById("output").innerHTML = 
     `
   <dl>
     <dd >${inObj.name}</dd>
 
-    <dd >£${inObj.plan}</dd>
+    <dd >${inObj.plan}</dd>
 
     <dd > ${inObj.weight} </dd>
 
     <dd >${inObj.cat}</dd>
 
-    <dd id = coachingresult>${inObj.coaching}</dd>
-    <dd id = competitionresult>${inObj.competition}</dd>
-
+    <dd id = coachingresult> ${inObj.coaching}</dd>
+    <dd id = competitionresult> ${inObj.competition}</dd>
+    <script>
+    
+  if (inObj.coaching === ""){document.getElementById(coachingresult).style.display = "none"}
+    else{document.getElementById(coachingresult).style.display = "block"}
+  if (inObj.competition ===""){document.getElementById(competitionresult).style.display = "none"}
+    else{document.getElementById(competitionresult).style.display = "block"}
+    
+    </script>
   </dl>
 `
-if (inObj.coaching === ""){document.getElementById(coachingresult).style.display = "none"}
-  else{document.getElementById(coachingresult).style.display = "block"}
-if (inObj.competition ===""){document.getElementById(competitionresult).style.display = "none"}
-  else{document.getElementById(competitionresult).style.display = "block"}
+
 
 }
